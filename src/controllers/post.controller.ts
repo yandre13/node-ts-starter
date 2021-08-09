29fc5310c1ecf54fe39f6bcd2b203306448347ab
Yandre <yandre13.elsf@gmail.com>
2021-08-08 23:35:25 -0500

    added try catch to controllers

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -7,79 +7,109 @@ import {
 } from 'services/post.service'
 
 export async function createPostHandler(req: Request, res: Response) {
- // @ts-ignore
- const userId = req.user._id
- const body = req.body
- const post = await createPost({...body, user: userId})
- return res.status(201).send({
-  success: true,
-  post,
- })
+ try {
+  // @ts-ignore
+  const userId = req.user._id
+  const body = req.body
+  const post = await createPost({...body, user: userId})
+  return res.status(201).send({
+   success: true,
+   post,
+  })
+ } catch (error) {
+  return res.status(400).send({
+   success: false,
+   error: error.message,
+  })
+ }
 }
 
 export async function updatePostHandler(req: Request, res: Response) {
- // @ts-ignore
- const userId = req.user._id
- // @ts-ignore
- const postId = req.params.id
- const update = req.body
- const post = await findPost({_id: postId})
- if (!post) {
-  return res.status(404).send({
+ try {
+  // @ts-ignore
+  const userId = req.user._id
+  // @ts-ignore
+  const postId = req.params.id
+  const update = req.body
+  const post = await findPost({_id: postId})
+  if (!post) {
+   return res.status(404).send({
+    success: false,
+    error: 'Post not found',
+   })
+  }
+  if (String(post.user) !== userId) {
+   return res.status(401).send({
+    success: false,
+    error: 'Unauthorized',
+   })
+  }
+  const updatedPost = await findAndUpdatePost({_id: postId}, update, {
+   new: true,
+  })
+  return res.status(200).send({
+   success: true,
+   post: updatedPost,
+  })
+ } catch (error) {
+  return res.status(400).send({
    success: false,
-   error: 'Post not found',
+   error: error.message,
   })
  }
- if (String(post.user) !== userId) {
-  return res.status(401).send({
-   success: false,
-   error: 'Unauthorized',
-  })
- }
- const updatedPost = await findAndUpdatePost({_id: postId}, update, {new: true})
- return res.status(200).send({
-  success: true,
-  post: updatedPost,
- })
 }
 
 export async function getPostHandler(req: Request, res: Response) {
- // @ts-ignore
- const postId = req.params.id
- const post = await findPost({_id: postId})
- if (!post) {
-  return res.status(404).send({
+ try {
+  // @ts-ignore
+  const postId = req.params.id
+  const post = await findPost({_id: postId})
+  if (!post) {
+   return res.status(404).send({
+    success: false,
+    error: 'Post not found',
+   })
+  }
+  return res.status(200).send({
+   success: true,
+   post,
+  })
+ } catch (error) {
+  return res.status(400).send({
    success: false,
-   error: 'Post not found',
+   error: error.message,
   })
  }
- return res.status(200).send({
-  success: true,
-  post,
- })
 }
 
 export async function deletePostHandler(req: Request, res: Response) {
- // @ts-ignore
- const userId = req.user._id
- // @ts-ignore
- const postId = req.params.id
- const post = await findPost({_id: postId})
- if (!post) {
-  return res.status(404).send({
+ try {
+  // @ts-ignore
+  const userId = req.user._id
+  // @ts-ignore
+  const postId = req.params.id
+  const post = await findPost({_id: postId})
+  if (!post) {
+   return res.status(404).send({
+    success: false,
+    error: 'Post not found',
+   })
+  }
+  if (String(post.user) !== userId) {
+   return res.status(401).send({
+    success: false,
+    error: 'Unauthorized',
+   })
+  }
+  await deletePost({_id: postId})
+  return res.status(200).send({
+   success: true,
+   message: 'Product deleted successfully',
+  })
+ } catch (error) {
+  return res.status(400).send({
    success: false,
-   error: 'Post not found',
+   error: error.message,
   })
  }
- if (String(post.user) !== userId) {
-  return res.status(401).send({
-   success: false,
-   error: 'Unauthorized',
-  })
- }
- await deletePost({_id: postId})
- return res.status(200).send({
-  success: true,
-  message: 'Product deleted successfully',
- })
 }
--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -9,49 +9,70 @@ import {
 import {sign} from 'utils/jwt.utils'
 
 export async function createSessionHandler(req: Request, res: Response) {
- //Validate email and password
- const user = await validatePassword(req.body)
- if (!user) {
-  return res.status(401).send({
+ try {
+  //Validate email and password
+  const user = await validatePassword(req.body)
+  if (!user) {
+   return res.status(401).send({
+    success: false,
+    error: 'Invalid username or password.',
+   })
+  }
+  //Create a session
+  const sessionMongoose = await createSession(
+   user._id,
+   req.get('User-Agent') || '',
+  )
+  const session = sessionMongoose.toJSON()
+  //Create access token
+  const accessToken = createAccessToken({user, session})
+  //Create refresh token
+  const refreshToken = sign(session, {expiresIn: '1y'})
+  //Send tokens
+  return res.status(201).send({
+   succes: true,
+   accessToken,
+   refreshToken,
+  })
+ } catch (error) {
+  return res.status(400).send({
    success: false,
-   error: 'Invalid username or password.',
+   error: error.message,
   })
  }
- //Create a session
- const sessionMongoose = await createSession(
-  user._id,
-  req.get('User-Agent') || '',
- )
- const session = sessionMongoose.toJSON()
- //Create access token
- const accessToken = createAccessToken({user, session})
- //Create refresh token
- const refreshToken = sign(session, {expiresIn: '1y'})
- //Send tokens
- return res.status(201).send({
-  succes: true,
-  accessToken,
-  refreshToken,
- })
 }
 
 export async function invalidateSessionHandler(req: Request, res: Response) {
- // @ts-ignore
- const {session: sessionId} = req.user
- await updateSession({_id: sessionId}, {valid: false})
+ try {
+  // @ts-ignore
+  const {session: sessionId} = req.user
+  await updateSession({_id: sessionId}, {valid: false})
 
- return res.status(200).send({
-  success: true,
-  message: 'logout successfully',
- })
+  return res.status(200).send({
+   success: true,
+   message: 'logout successfully',
+  })
+ } catch (error) {
+  return res.status(400).send({
+   success: false,
+   error: error.message,
+  })
+ }
 }
 
 export async function getSessiosnHandler(req: Request, res: Response) {
- // @ts-ignore
- const userId = req.user?._id
- const sessions = await findSessions({user: userId, valid: true})
- return res.status(200).send({
-  success: true,
-  sessions,
- })
+ try {
+  // @ts-ignore
+  const userId = req.user?._id
+  const sessions = await findSessions({user: userId, valid: true})
+  return res.status(200).send({
+   success: true,
+   sessions,
+  })
+ } catch (error) {
+  return res.status(400).send({
+   success: false,
+   error: error.message,
+  })
+ }
 }
